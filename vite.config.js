6ee591e5fc170f2f289f0b9b6c7ba7397dedbf08
ismail-ismail123 <thearchitecturecorporation@gmail.com>
2024-10-11 16:15:11 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ms/',
+  base: '/ms-creation-tech-final/',
   plugins: [react()],
 })
